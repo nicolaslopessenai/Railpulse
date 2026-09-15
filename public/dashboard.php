f6bc40c089b122f99b7774bdd5eb05d30cd4a0f2
arthur <arthur_vieira1@estudante.sesisenai.org.br>
2026-09-15 11:41:45 -0300

    refatoração-dashboar-public-snake_case-e-padronização-de-class

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,12 +1,9 @@
 <?php
 include '../infra/conexao.php';
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,35 +13,26 @@ include '../infra/conexao.php';
 
 <body>
     <nav class="navegacao">
-        <div class="container-menu">
+        <div class="container_menu">
             <div class="logo">Rail<span>Pulse</span></div>
             <div class="paginas"><a href="dashboard.php" class="active">PAINEL</a></div>
             <div class="paginas"><a href="sensores.php">SENSORES</a></div>
             <div class="paginas"><a href="trens.php">TRENS</a></div>
             <div class="paginas"><a href="rotas.php">ROTAS</a></div>
             <div class="paginas"><a href="relatorios.php">RELATÓRIOS</a></div>
-            <div class="topbar-info">
-                <span id="info-matricula" class="topbar-matricula"></span>
+            <div class="topbar_info">
+                <span id="info_matricula" class="topbar_matricula"></span>
                 <a href="../index.php" class="paginas">SAIR</a>
             </div>
-
         </div>
     </nav>
 
+    <main class="main_content">
+        <h2 class="page_title" id="boas_vindas">Bem-vindo</h2>
+        <hr class="divider"><br>
 
-    <div class="app-container">
-        <header class="topbar">
-            
-        </header>
-
-
-        <main class="main-content">
-            <h2 class="page-title" id="boas-vindas">Bem-vindo</h2>
-            <hr class="divider"><br>
-
-            <div class="section-title light" style="margin-top: 0;">RASTREAMENTO DE LOCALIZAÇÃO</div>
-        </main>
-    </div>
+        <div class="section_title light" style="margin-top: 0;">RASTREAMENTO DE LOCALIZAÇÃO</div>
+    </main>
 
     <script src="../script/dashboard.js"></script>
 </body>
