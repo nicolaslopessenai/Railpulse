019cb2c0461c3fb9be15869cbf0555d8753c6f05
gabriel <gabriel_ostrovski1@estudante.sesisenai.org.br>
2026-09-15 11:23:06 -0300

    refatoração-relatorios-snake_case

--- public/relatorios.php
+++ public/relatorios.php
@@ -14,15 +14,15 @@ include '../infra/conexao.php';
 </head>
 <body>
     <nav class="navegacao">
-            <div class="container-menu">
+            <div class="container_menu">
                 <div class="logo">Rail<span>Pulse</span></div>
                 <div class="paginas"><a href="dashboard.php" >PAINEL</a></div>
                 <div class="paginas"><a href="sensores.php">SENSORES</a></div>
                 <div class="paginas"><a href="trens.php">TRENS</a></div>
                 <div class="paginas"><a href="rotas.php">ROTAS</a></div>
                 <div class="paginas"><a href="relatorios.php" class="active">RELATÓRIOS</a></div>
-                <div class="topbar-info">
-                    <span id="info-matricula" class="topbar-matricula"></span>
+                <div class="topbar_info">
+                    <span id="info_matricula" class="topbar_matricula"></span>
                     <a href="../index.php" class="paginas">SAIR</a>
                 </div>
 
