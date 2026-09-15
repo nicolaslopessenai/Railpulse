8b413a113876915c1e087dd94387a069081b8f91
arthur <arthur_vieira1@estudante.sesisenai.org.br>
2026-09-15 11:44:01 -0300

    refatoração-login-snake_case-e-padronização-de-class

--- public/login.php
+++ public/login.php
@@ -14,17 +14,17 @@ include '../infra/conexao.php';
     <link rel="stylesheet" href="../assets/style/style.css">
 </head>
 
-<body class="body-login">
+<body class="body_login">
         <main>
-        <div class="logo">Rail<span>Pulse</span></div>
-        <div class="e-form">
+                    <div class="logo">Rail<span>Pulse</span></div>
+        <div class="e_form">
             <h2 id="titulo">Login</h2>
             <form id="form" method="POST" action="../infra/processar_login.php">
-                <div id="e-email" class="conjunto">
+                <div id="e_email" class="conjunto">
                     <label for="email">Email:</label>
                     <input type="email" name="email" id="email" placeholder="Email" required>
                 </div>
-                <div id="e-senha" class="conjunto">
+                <div id="e_senha" class="conjunto">
                     <label for="senha">Senha:</label>
                     <input type="password" name="senha" id="senha" placeholder="Senha" required>
                 </div>
